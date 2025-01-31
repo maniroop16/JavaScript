b4722447ff38c4342c703fc3af8043f52a60b3fd
LTIsaimaniroop <saimaniroop.10687590@ltimindtree.com>
2025-01-31 10:34:20 +0530

    First commit

---
+++ test.js
@@ -0,0 +1,14 @@
+// console.log("mani");
+// let var1 = "";
+// let var2;
+// let var3;
+// console.log();
+
+// console.table([var1, var2, var3]);
+
+
+const welcome  = function(){
+    console.log("hellooooo")
+}
+a = "sai"
+console.log(a.length);
